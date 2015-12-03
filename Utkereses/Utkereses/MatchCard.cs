7d0d31d780f851c3057d92052f2cf23a7cfb87a3
Najt <duna.gergely@gmail.com>
2015-12-03 14:45:29 +0100

    Getting Started
    Egy beadandó féléves feladat. Beolvass egy fájlt, és megállapítja a
    legtöbbször ciklikusan előforduló szót, és kiírja az előforduló
    változatait

---
+++ Utkereses/Utkereses/MatchCard.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utkereses
+{
+    class MatchCard
+    {
+        public List<string> marker = new List<string>();
+        public List<string> matches = new List<string>();
+
+        public MatchCard(string data) {
+            marker.Add(data);
+            marker.Add(reverse(data));
+            for (int i = 0; i < data.Length; i++)
+            {
+                move(ref data);
+                marker.Add(data);
+                marker.Add(reverse(data));
+            }
+        }
+        public void getMatches(ref List<string> list) {
+            int i = 0;
+            while (i < list.Count) {
+                if (marker.Contains(list[i]))
+                {
+                    matches.Add(list[i]);
+                    list.RemoveAt(i);
+                }
+                else {
+                    i++;
+                }
+            }
+        }
+        private void move(ref string word) {
+            char firstLetter = word.ToCharArray()[0];
+            char[] chWord = word.ToCharArray();
+            for (int i = 0; i < chWord.Length-1; i++)
+            {
+                chWord[i] = chWord[i + 1];
+            }
+            chWord[chWord.Length - 1] = firstLetter;
+            word = new string(chWord);
+        }
+        private string reverse(string word) {
+            char[] chWord = word.ToCharArray();
+            int j = chWord.Length - 1;
+            for (int i = 0; i < chWord.Length/2; i++)
+            {
+                char temp = chWord[i];
+                chWord[i] = chWord[j];
+                chWord[j] = temp;
+                j--;
+            }
+            return new string(chWord);
+        }
+        
+    }
+}
---
+++ Utkereses/Utkereses/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utkereses
+{
+    class Program
+    {
+        static List<string> dataList = new List<string>();
+        static List<MatchCard> cardList = new List<MatchCard>();
+
+        static void Main(string[] args)
+        {
+            string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string path = Path.GetFullPath(home + "\\Desktop\\ut.BE");
+            string[] input;
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    input = sr.ReadToEnd().Split(' ');
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        dataList.Add(input[i]);
+                    }
+                    start();
+                    sr.Close();
+                }
+            }
+            catch (Exception e){
+                Console.WriteLine(e.Message);
+            }
+            
+            
+        }
+        static void start() {
+            while (dataList.Count != 0)
+            {
+                MatchCard tempCard = new MatchCard(dataList[0]);
+                tempCard.getMatches(ref dataList);
+                cardList.Add(tempCard);
+            }
+            int imax = MaxKiv();
+            for (int i = 0; i < cardList[imax].matches.Count; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("Előfordulási száma: "+cardList[imax].matches.Count+"\n\nA szó:");
+                    Console.WriteLine(cardList[imax].matches[i]);
+                    Console.WriteLine("\nVáltozatai:");
+                }
+                Console.Write(cardList[imax].matches[i]);
+                if (i != cardList[imax].matches.Count - 1) {
+                    Console.Write(", ");
+                }
+            }
+            Console.Write("\n\n");
+        }
+        static int MaxKiv() {
+            int imax = 0;
+            for (int i = 0; i < cardList.Count; i++)
+            {
+                if(cardList[i].matches.Count> cardList[imax].matches.Count) {
+                    imax = i;
+                }
+            }
+            return imax;
+        }
+    }
+}
